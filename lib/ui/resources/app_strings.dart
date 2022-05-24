98d09abf8efa3278d93f493066f60105734dbbef
Omar.abdelAzim <omar.abdelazeem87@gmail.com>
2022-05-24 14:32:44 +0200

    Add some translations

--- lib/ui/resources/app_strings.dart
+++ lib/ui/resources/app_strings.dart
@@ -14,7 +14,6 @@ abstract class AppStrings {
   static const String technicalSupport = "technicalSupport";
   static const String diseaseDetails = "diseaseDetails";
 
-
   static const String signUp = "signUp";
   static const String validMobile = "validMobile";
   static const String appName = "appName";
@@ -34,64 +33,114 @@ abstract class AppStrings {
   static const String pathogens = "pathogens";
   static const String activities = "activities";
 
+  static const String rateApp = "rateApp";
+  static const String signOut = "signOut";
 
-  static const String  rateApp ="rateApp";
-  static const String  signOut ="signOut";
+  static const String chat = "chat";
+  static const String welcome = "welcome";
+  static const String details = "details";
+  static const String plantDetails = "plantDetails";
+  static const String choosePlantType = "choosePlantType";
+  static const String appDetails = "appDetails";
+  static const String camera = "camera";
+  static const String gallery = "gallery";
+  static const String supportedPlant = "supportedPlant";
+
+  static const String strawberry = "strawberry";
+  static const String tomatoes = "tomatoes";
+  static const String apples = "apples";
+  static const String corn = "corn";
+  static const String bellPepper = "bellPepper";
+  static const String cherry = "cherry";
+  static const String potato = "potato";
+  static const String peach = "peach";
+  static const String citrus = "citrus";
+  static const String grape = "grape";
+
+  static const String farmingAdvices = "farmingAdvices";
+  static const String compost = "compost";
+  static const String farmingBasics = "farmingBasics";
+  static const String vegetablesPlanting = "vegetablesPlanting";
+  static const String fruitsPlanting = "fruitsPlanting";
+  static const String ornamentalPlant = "ornamentalPlant";
+  static const String plantPests = "plantPests";
+  static const String plantBenefits = "plantBenefits";
+
+  static const String roofPreparation = "roofPreparation";
+  static const String roofPreparation1 = "roofPreparation1";
+  static const String roofPreparation2 = "roofPreparation2";
+  static const String roofPreparation3 = "roofPreparation3";
+  static const String roofPreparation4 = "roofPreparation4";
+  static const String roofPreparation5 = "roofPreparation5";
+  static const String roofPreparation6 = "roofPreparation6";
+  static const String roofPreparation7 = "roofPreparation7";
+
+  static const String whyDoIPlan = "whyDoIPlan";
+  static const String whyDoIPlan1 = "whyDoIPlan1";
+  static const String whyDoIPlan2 = "whyDoIPlan2";
+  static const String whyDoIPlan3 = "whyDoIPlan3";
+  static const String whyDoIPlan4 = "whyDoIPlan4";
+  static const String whyDoIPlan5 = "whyDoIPlan5";
+  static const String whyDoIPlan6 = "whyDoIPlan6";
+  static const String whyDoIPlan7 = "whyDoIPlan7";
+  static const String whyDoIPlan8 = "whyDoIPlan8";
+  static const String whyDoIPlan9 = "whyDoIPlan9";
+
+  /// Farming Tips
+  static const String farmingTips1 = "farmingTips1"   ;
+  static const String farmingTips2 = "farmingTips2"   ;
+  static const String farmingTips3 = "farmingTips3"   ;
+  static const String farmingTips4 = "farmingTips4"   ;
+  static const String farmingTips5 = "farmingTips5"   ;
+  static const String farmingTips6 = "farmingTips6"   ;
+  static const String farmingTips7 = "farmingTips7"   ;
+  static const String farmingTips8 = "farmingTips8"   ;
+  static const String farmingTips9 = "farmingTips9"   ;
+  static const String farmingTips10 = "farmingTips10";
+  static const String farmingTips11 = "farmingTips11";
+  static const String farmingTips12 = "farmingTips12";
+
+  ///Decoration Plants
+  //Aglaonema
+  static const String decorationPlantsAglaonemaName  = "decorationPlantsAglaonemaName"   ;
+  static const String decorationPlantsAglaonemaSuitableAtmosphere = "decorationPlantsAglaonemaSuitableAtmosphere";
+  static const String decorationPlantsAglaonemaReproduction = "decorationPlantsAglaonemaReproduction";
+  static const String decorationPlantsAglaonemaCare  = "decorationPlantsAglaonemaCare";
+
+  //Bamboo
+  static const String decorationPlantsBambooName  = "decorationPlantsBambooName";
+  static const String decorationPlantsBambooReproduction  = "decorationPlantsBambooReproduction";
+  static const String decorationPlantsBambooCare  = "decorationPlantsBambooCare";
+
+  /// Plant pests
+  //Old needle
+
+  static const String plantPestsOldNeedle  = "plantPestsOldNeedle";
+  static const String plantPestsOldNeedleWaysToPreventOldNeedle  = "plantPestsOldNeedleWaysToPreventOldNeedle";
+
+  //Mites
+  static const String plantPestsMites     = "plantPestsMites";
+  static const String plantPestsAntiMites  = "plantPestsAntiMites";
+
+  /// Compost
+  // NPK
+  static const String compostNPK          = "compostNPK";
+  static const String compostNPKNitrogen     = "compostNPKNitrogen";
+  static const String compostNPKPhosphorous      = "compostNPKPhosphorous";
+  static const String compostNPKPotassium       = "compostNPKPotassium";
 
 
-  static const String  chat ="chat";
-  static const String  welcome ="welcome";
-  static const String  details ="details";
-  static const String  plantDetails ="plantDetails";
-  static const String  choosePlantType ="choosePlantType";
-  static const String  appDetails ="appDetails";
-  static const String  camera ="camera";
-  static const String  gallery ="gallery";
-  static const String  supportedPlant="supportedPlant";
-
-  static const String  strawberry="strawberry";
-  static const String  tomatoes="tomatoes";
-  static const String  apples="apples";
-  static const String  corn="corn";
-  static const String  bellPepper="bellPepper";
-  static const String  cherry="cherry";
-  static const String  potato="potato";
-  static const String  peach="peach";
-  static const String  citrus="citrus";
-  static const String  grape="grape";
-
-  static const String  farmingAdvices="farmingAdvices";
-  static const String  compost="compost";
-  static const String  farmingBasics="farmingBasics";
-  static const String  vegetablesPlanting="vegetablesPlanting";
-  static const String  fruitsPlanting="fruitsPlanting";
-  static const String  ornamentalPlant="ornamentalPlant";
-  static const String  plantPests="plantPests";
-  static const String  plantBenefits="plantBenefits";
-
-  static const String  roofPreparation="roofPreparation";
-  static const String  roofPreparation1="roofPreparation1";
-  static const String  roofPreparation2="roofPreparation2";
-  static const String  roofPreparation3="roofPreparation3";
-  static const String  roofPreparation4="roofPreparation4";
-  static const String  roofPreparation5="roofPreparation5";
-  static const String  roofPreparation6="roofPreparation6";
-  static const String  roofPreparation7="roofPreparation7";
+  //Yeast Feeding
+  static const String compostYeastFeeding                               = "compostYeastFeeding";
+  static const String compostYeastBenefits                           = "compostYeastBenefits";
+  static const String compostIsYeastIrrigationBetterOrSprinkling = "compostIsYeastIrrigationBetterOrSprinkling";
 
 
-  static const String  whyDoIPlan="whyDoIPlan";
-  static const String  whyDoIPlan1="whyDoIPlan1";
-  static const String  whyDoIPlan2="whyDoIPlan2";
-  static const String  whyDoIPlan3="whyDoIPlan3";
-  static const String  whyDoIPlan4="whyDoIPlan4";
-  static const String  whyDoIPlan5="whyDoIPlan5";
-  static const String  whyDoIPlan6="whyDoIPlan6";
-  static const String  whyDoIPlan7="whyDoIPlan7";
-  static const String  whyDoIPlan8="whyDoIPlan8";
-  static const String  whyDoIPlan9="whyDoIPlan9";
-
-
-
-
-
+  ///Plant benefits
+ // onion
+  static const String onion = "onion";
+  static const String plantBenefitsOnion = "plantBenefitsOnions";
+  // dates
+  static const String dates = "dates";
+  static const String plantBenefitsDates = "plantBenefitsDates";
 }
